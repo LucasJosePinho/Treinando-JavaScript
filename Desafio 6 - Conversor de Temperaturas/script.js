871a65883bab62d12f94ebac1549192e61ecfe25
Lucas Jose Pereira Reis Rodrigues de Pinho <lucasreispinho@hotmail.com>
2022-09-08 22:29:50 -0300

    Ajuste

--- Desafio 6 - Conversor de Temperaturas/script.js
+++
@@ -1,5 +0,0 @@
-function conv() {
-    var temp = Number(window.prompt('Digite uma temperatura em ºC (Celsius)'))
-    var texto = window.document.getElementById('texto')
-    texto.innerHTML = `<p><font size="5"><strong> A temperatura de ${temp}ºC, corresponde a... </strong></font></p>\n<p>${temp+273}ºK (Kelvin)</p>\n<p>${temp*1.8+32}ºF (Fahrenheit)</p>`
-}
